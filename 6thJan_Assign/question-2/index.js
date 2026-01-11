d3a36015606b56ecd7df9788262fa7a777ac6501
kpriyanka <kusupriyanka04@gmail.com>
2026-01-11 15:21:43 +0530

    Node.js Architecture, Event Loop & Modular System

---
+++ 6thJan_Assign/question-2/index.js
@@ -0,0 +1,7 @@
+import { checkPrime } from "./math";
+
+console.log(checkPrime(2));
+console.log(checkPrime(11));
+console.log(checkPrime(15));
+console.log(checkPrime(1));
+console.log(checkPrime(97));
